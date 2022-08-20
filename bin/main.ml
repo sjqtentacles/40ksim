1a142df241507b80f49208ce1a3e3c44f522f9c8
sjqtentacles <programminglinguist@gmail.com>
2022-08-19 22:37:18 -0700

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,29 @@
+open Raylib
+
 let window_width = 800
 let window_height = 450
 
-type species = 
+type _species = 
   | Unknown
   | Human
   | Mekkan
-  | 
 
 let setup () =
   Raylib.init_window window_width window_height "raylib [core] example - basic window";
   Raylib.set_target_fps 60
 
+let draw_background () = 
+  begin
+    clear_background Color.gold;
+    draw_rectangle 10 10 15 15 Color.darkgreen;
+  end
+
 let draw () = 
   let open Raylib in 
   begin_drawing ();
-  clear_background Color.gold;
+  draw_background ();
   draw_text "Ork Hunter" 100 100 25 Color.black;
 end_drawing()
-
-
 
 let rec loop () =
   match Raylib.window_should_close () with
