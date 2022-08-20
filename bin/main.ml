579dd862308eaf77fcbb2382842ede9a1990aed4
sjqtentacles <programminglinguist@gmail.com>
2022-08-19 20:55:17 -0700

    init commit

---
+++ bin/main.ml
@@ -0,0 +1,30 @@
+let window_width = 800
+let window_height = 450
+
+type species = 
+  | Unknown
+  | Human
+  | Mekkan
+  | 
+
+let setup () =
+  Raylib.init_window window_width window_height "raylib [core] example - basic window";
+  Raylib.set_target_fps 60
+
+let draw () = 
+  let open Raylib in 
+  begin_drawing ();
+  clear_background Color.gold;
+  draw_text "Ork Hunter" 100 100 25 Color.black;
+end_drawing()
+
+
+
+let rec loop () =
+  match Raylib.window_should_close () with
+  | true -> Raylib.close_window ()
+  | false ->
+      draw();
+      loop ()
+
+let () = setup () |> loop
