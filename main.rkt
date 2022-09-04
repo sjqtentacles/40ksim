dddab21a131532b362a3590bb7156f82d96778bb
wikibaus <programminglinguist@gmail.com>
2022-09-03 18:10:04 -0700

    adding comps and ents etc next comes lenses

--- main.rkt
+++ main.rkt
@@ -6,6 +6,12 @@
 
 (struct size (radius) #:transparent)
 
+(struct component (name attributes) #:transparent)
+
+(struct entity (id components) #:transparent)
+
+(struct world (entities components systems db) #:transparent)
+
 ; move : posn posn -> posn
 (define (move from vec2)
   (let ([x (+ (posn-x from) (posn-x vec2))]
@@ -23,11 +29,33 @@
   (let ([dist (distance p1 p2)])
     (<= dist (+ s1 s2))))
 
-; Index mapping entity IDs to components
-(define entities (make-hash))
+; Entities - Index mapping entity IDs to components
+; Components - Index mapping component names to entity ids
 
-; Index mapping component names to entity ids
-(define components  (make-hash))
+; Returns an empty world
+(define (new-world)
+  (world (make-hash) (make-hash) null null))
+
+; Returns a uuid string
+(define (add-new-entity w)
+  ((uuid-string))
+
+; Returns a new world after running all systems in order
+(define (run-all-systems world)
+  (let ([systems (world-systems world)])
+    (foldl (lambda (w sys) (sys w)) world systems)))
+
+(define (new-component)
+  (make-hash))
+
+(define (add-component w ent-id comp)
+  (let ([ents (world-entities w)]
+        [ent-comps (hash-ref ents ent-id)]
+        [new-ents (hash-set ent-comps ent-id (cons comp ent-comps))]
+        [components (world-components w)]
+        [new-components (hash-update components 
+        
+  
 
 (define start (posn 0 0))
 (define movedir (posn 10 8))
