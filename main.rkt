b6a2b172da7ea21f6f509f02228270276d38b2fd
wikibaus <programminglinguist@gmail.com>
2022-09-04 22:18:08 -0700

    fixing ecs

--- main.rkt
+++ main.rkt
@@ -1,69 +1,46 @@
 #lang racket
 
-(require threading math)
+(require threading math lens uuid)
 
-(struct posn (x y) #:transparent)
+(struct component (type attrs) #:transparent)
 
-(struct size (radius) #:transparent)
-
-(struct component (name attributes) #:transparent)
-
-(struct entity (id components) #:transparent)
-
-(struct world (entities components systems db) #:transparent)
-
-; move : posn posn -> posn
-(define (move from vec2)
-  (let ([x (+ (posn-x from) (posn-x vec2))]
-        [y (+ (posn-y from) (posn-y vec2))])
-    (posn x y)))
-
-; distance : posn posn -> Number
-(define (distance from to)
-  (let ([dx (- (posn-x from) (posn-x to))]
-        [dy (- (posn-y from) (posn-y to))])
-    (sqrt (+ (expt dx 2) (expt dy 2)))))
-
-; touching? : posn Natural posn Natural -> Boolean
-(define (touching? p1 s1 p2 s2)
-  (let ([dist (distance p1 p2)])
-    (<= dist (+ s1 s2))))
-
-; Entities - Index mapping entity IDs to components
-; Components - Index mapping component names to entity ids
-
-; Returns an empty world
 (define (new-world)
-  (world (make-hash) (make-hash) null null))
+  (hash
+   'step 0 ; a world step counter, think of it like time
+   'entities null ; Entities hashmap UUIDs to Components
+   'components null ; Components hashmap component "types" to entity UUIDs
+   'systems null ; Systems list of functions that work on entities/components
+   'db null)) ; db is a caching queue for entity addition/removal etc
+
+(define (get-entity world id)
+  (lens-view (hash-ref-nested-lens 'entities id) world))
 
 ; Returns a uuid string
-(define (add-new-entity w)
-  ((uuid-string))
+(define (add-new-entity world)
+  (lens-set (hash-ref-lens 'entities) world (uuid-string)))
+
+(define (update-world-step w)
+  (lens-transform (hash-ref-lens 'step) w add1))
 
 ; Returns a new world after running all systems in order
 (define (run-all-systems world)
-  (let ([systems (world-systems world)])
-    (foldl (lambda (w sys) (sys w)) world systems)))
+  (let* ([next-world (update-world-step world)]
+        [systems (hash-ref next-world 'systems)])
+    (foldl (λ (sys w) (sys w)) next-world systems)))
 
-(define (new-component)
-  (make-hash))
+(define (new-component type)
+  (hash 'type type))
 
-(define (add-component w ent-id comp)
-  (let ([ents (world-entities w)]
-        [ent-comps (hash-ref ents ent-id)]
-        [new-ents (hash-set ent-comps ent-id (cons comp ent-comps))]
-        [components (world-components w)]
-        [new-components (hash-update components 
-        
-  
+(define (add-new-system world system)
+  (lens-transform
+   (hash-ref-lens 'systems)
+   world
+   (λ (systems) (cons system systems))))
 
-(define start (posn 0 0))
-(define movedir (posn 10 8))
-(define secondmove (posn -5 -6))
-(define testsize (size 0))
+(define (main)
+  (~> (new-world)
+      add-new-entity
+      (add-new-system (λ (w) (begin (print "hi") w)))
+      run-all-systems))
 
-(display
- (~> start
-     (move movedir)
-     (move secondmove)
-     (distance (posn 2 2))))
+(main)
