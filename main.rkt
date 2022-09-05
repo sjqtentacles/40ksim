7bce7154ad568711311a8da9146af31ad0dae428
wikibaus <programminglinguist@gmail.com>
2022-09-05 14:34:34 -0700

    think it works now

--- main.rkt
+++ main.rkt
@@ -7,22 +7,39 @@
 (define (new-world)
   (hash
    'step 0 ; a world step counter, think of it like time
-   'entities (make-hash) ; Entities hashmap UUIDs to Components
-   'components (make-hash) ; Components hashmap component "types" to entity UUIDs
+   'entities (hash) ; Entities hashmap UUIDs to Components
+   'components (hash) ; Components hashmap component "types" to entity UUIDs
    'systems null ; Systems list of functions that work on entities/components
    'db null)) ; db is a caching queue for entity addition/removal etc
 
+; world uuid -> world
 (define (get-entity world id)
   (lens-view (hash-ref-nested-lens 'entities id) world))
 
-(define (add-component-to-entities world comp id)
-  (let ([l (hash-ref-nested-lens 'entities id)])
-    (lens-transform l world (λ (comps) (cons comp comps)))))
+; world uuid -> boolean
+(define (entity-exists? world id)
+  (hash-has-key? (lens-view (hash-ref-lens 'entities) world) id))
 
-; Returns a uuid string
+; world component uuid -> world
+(define (add-component-to-entity world comp id)
+  (let* ([ent-id-lens (hash-ref-nested-lens 'entities id)] ; getting entity uuid's hashmap
+        [comp-lens (hash-ref-lens 'components)] ; getting components hashmap
+        [with-updated-components ; inserting new comp into components hashmap
+            (lens-transform
+             comp-lens
+             world
+             (λ (comps-hash) ; using a hash-update func to do it
+               (hash-update comps-hash
+                            (component-type comp)
+                            (λ (ents) (cons id ents))
+                            null)))])
+    (lens-transform ent-id-lens with-updated-components (λ (comps) (cons comp comps)))))
+
+; world uuid -> world
 (define (add-new-entity world id)
   (lens-set (hash-ref-lens 'entities) world (hash id null)))
 
+; world -> world
 (define (update-world-step w)
   (lens-transform (hash-ref-lens 'step) w add1))
 
@@ -32,18 +49,20 @@
         [systems (hash-ref next-world 'systems)])
     (foldl (λ (sys w) (sys w)) next-world systems)))
 
+; world (world -> world) -> world
 (define (add-new-system world system)
   (lens-transform
    (hash-ref-lens 'systems)
    world
    (λ (systems) (cons system systems))))
 
+
 (define test-uuid (uuid-string))
 
 (define (main)
   (~> (new-world)
       (add-new-entity _ test-uuid)
-      (add-component-to-entities _ (component 'spell (hash 'name "fire" 'damage 3)) test-uuid)
+      (add-component-to-entity _ (component 'spell (hash 'name "fire" 'damage 3)) test-uuid)
       (add-new-system _ (λ (w) (begin (print "hi") w)))
       run-all-systems))
 
