4b646c6871abe0dcc73b1c6a827d7c958a630480
wikibaus <programminglinguist@gmail.com>
2022-09-05 17:23:25 -0700

    adding contracts

--- main.rkt
+++ main.rkt
@@ -1,13 +1,14 @@
 #lang racket
 
-(require threading math lens uuid)
+(require threading math lens uuid racket/contract)
 
 (struct component (type attrs) #:transparent)
 
 (struct system (name logic) #:transparent)
 
 ; makes a new empty hashmap for the world state
-(define (new-world)
+(define/contract (new-world)
+  (-> hash?)
   (hash
    'step 0 ; a world step counter, think of it like time
    'entities (hash) ; Entities hashmap UUIDs to Components
@@ -16,15 +17,18 @@
    'db null)) ; db is a caching queue for entity addition/removal etc
 
 ; world uuid -> world
-(define (get-entity world id)
+(define/contract (get-entity world id)
+  (-> hash? string? hash?)
   (lens-view (hash-ref-nested-lens 'entities id) world))
 
 ; world uuid -> boolean
-(define (entity-exists? world id)
+(define/contract (entity-exists? world id)
+  (-> hash? string? hash?)
   (hash-has-key? (lens-view (hash-ref-lens 'entities) world) id))
 
 ; world component uuid -> world
-(define (add-component-to-entity world comp id)
+(define/contract (add-component-to-entity world comp id)
+  (-> hash? component? string? hash?)
   (let* ([ent-id-lens (hash-ref-nested-lens 'entities id)] ; getting entity uuid's hashmap
         [comp-lens (hash-ref-lens 'components)] ; getting components hashmap
         [with-updated-components ; inserting new comp into components hashmap
@@ -39,46 +43,54 @@
     (lens-transform ent-id-lens with-updated-components (λ (comps) (cons comp comps)))))
 
 ; world uuid -> world
-(define (add-new-entity world id)
+(define/contract (add-new-entity world id)
+  (-> hash? string? hash?)
   (lens-set (hash-ref-lens 'entities) world (hash id null)))
 
 ; world -> world
-(define (update-world-step w)
+(define/contract (update-world-step w)
+  (-> hash? hash?)
   (lens-transform (hash-ref-lens 'step) w add1))
 
 ; Returns a new world after running all systems in order
-(define (run-all-systems world)
+(define/contract (run-all-systems world)
+  (-> hash? hash?)
   (let* ([next-world (update-world-step world)]
         [systems (hash-ref next-world 'systems)])
     (foldl (λ (sys w) ((system-logic sys) w)) next-world systems)))
 
 ; world -> (world -> world) -> world
-(define (add-new-system world system)
+(define/contract (add-new-system world system)
+  (-> hash? system? hash?)
   (lens-transform
    (hash-ref-lens 'systems)
    world
    (λ (systems) (cons system systems))))
 
 ; world -> uuid -> world
-(define (remove-entity-from-entities world id)
+(define/contract (remove-entity-from-entities world id)
+  (-> hash? string? hash?)
   (lens-transform
    (hash-ref-lens 'entities)
    world
    (λ (ents-map) (hash-remove ents-map id))))
 
 ; world -> component-type -> world
-(define (remove-component-type-from-components world comp-type)
+(define/contract (remove-component-type-from-components world comp-type)
+  (-> hash? (or/c string? symbol?) hash?)
   (lens-transform
    (hash-ref-lens 'components)
    world
    (λ (comps-map) (hash-remove comps-map comp-type))))
 
 ; components list -> component-type -> components list
-(define (remove-components-with-type comps-list comp-type)
+(define/contract (remove-components-with-type comps-list comp-type)
+  (-> (listof (or/c string? symbol?)) (or/c string? symbol?) (listof (or/c string? symbol?)))
   (filter (λ (c) (not (equal? (component-type c) comp-type))) comps-list))
 
 ; world -> uuid -> component-type -> world
-(define (remove-components-from-entity-with-type world ent-id comp-type)
+(define/contract (remove-components-from-entity-with-type world ent-id comp-type)
+  (-> hash? string? (or/c string? symbol?) hash?)
   (lens-transform
    (hash-ref-nested-lens 'entities ent-id)
    world
@@ -86,7 +98,8 @@
 
 (define test-uuid (uuid-string))
 
-(define (main)
+(define/contract (main)
+  (-> hash?)
   (~> (new-world)
       (add-new-entity _ test-uuid)
       (add-component-to-entity _ (component 'spell (hash 'name "fire" 'damage 3)) test-uuid)
