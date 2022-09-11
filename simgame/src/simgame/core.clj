61deab52bd929e69f1e3c62b6863b5ae53b7bc07
wikibaus <programminglinguist@gmail.com>
2022-09-11 15:42:21 -0700

    wip to clojure

---
+++ simgame/src/simgame/core.clj
@@ -0,0 +1,26 @@
+(ns simgame.core
+  (:gen-class)
+  (:require 
+    [simgame.components :as comps]
+    [simgame.entities :as ents]))
+
+(defn refresh []
+  (use 'simgame.core :reload))
+
+(defn refresh-all []
+  (use 'simgame.core :reload-all))
+
+(defn new-world []
+  {
+    :entities nil
+    :components nil
+    :systems nil
+    :db nil
+    :age 0
+  })
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (comps/hi))
